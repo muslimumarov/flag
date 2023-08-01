a0a76b88587721730348090f4d89f450d56e71ad
muslimumarov <norbekumarov613@gmail.com>
2023-08-01 19:08:13 +0500

    uz

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-var elSwitchButton = document.querySelector(".site-header__button");
-
-elSwitchButton.addEventListener("click", function(){
- 
-  document.body.classList.toggle("dark")
+var elButtonHd = document.querySelector(".site-header__button");
+var elSelectHr = document.querySelector("placeholder");
+var elHeaderLogo = document.querySelector(".header-logo");
+elButtonHd.addEventListener("click" , function(){
+  document.body.classList.toggle("dark");
+ elSelectHr.document.classList.toggle("dark");
+ elHeaderLogo.document.classList.toggle("dark");
+})
